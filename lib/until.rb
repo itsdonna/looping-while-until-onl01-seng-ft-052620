4cfd7032f7c319d9f10bb9ebe9c357be7e919e2c
Donna Torres <todonnatorres@icloud.com>
2020-06-01 21:54:48 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,10 @@
-def using_until do
+def using_until 
   levitation_force = 6
   #your code here
-  puts "Wingardium Leviosa"
-  if using_until >= 10
-    break
+    until levitation_force = 10
+    puts "Wingardium Leviosa"
+    levitation_force +=1
   end
-    
 end
+ 
 
